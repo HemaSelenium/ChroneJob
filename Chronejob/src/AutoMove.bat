b42504ea871e11d03bf72c7ad701c9f540135877
HemaSelenium <hema814058@gmail.com>
2019-09-15 12:34:02 +0530

    first commit

---
+++ Chronejob/src/AutoMove.bat
@@ -0,0 +1,10 @@
+@echo off
+
+
+set "source=C:\Users\Hema Sumanjali\OneDrive - Mahindra & Mahindra Ltd\Test"
+
+set "destination=C:\Users\Hema Sumanjali\git\AutoinspektAPP\AutoinspektApp\APP"
+
+robocopy "%source%" "%destination%" /mov 
+
+
